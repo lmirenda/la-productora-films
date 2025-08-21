68da0fac5fdf71070160e58228b5279ad129b564
Luca Mirenda <lucamateomirenda@gmail.com>
2025-08-21 13:34:34 -0300

    Update apple

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -56,7 +56,7 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
+    apple: '/favicon.ico',
   },
   alternates: {
     canonical: '/',
